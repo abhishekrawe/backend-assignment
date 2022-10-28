5c06faf6cc1d4f695059367e55fda9bdcb538dff
abhishekrawe <rajputabhisheksingh53@gmail.com>
2022-10-28 09:58:22 +0530

    initail commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+import React , { useState , useEffect} from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App=()=> {
+  
+  const [fake, setFake] = useState([]);
+  console.log(fake);
+  useEffect(() => {
+    fakestore();
+  },[])
+
+  const fakestore = async() => {
+    const response = await fetch("https://fakestoreapi.com/products");
+    //console.log(response);
+    const jsonData = await response.json();
+    //console.log(jsonData);
+    setFake(jsonData);
+  }
+  //fakestore();
+   return(
+    <>
+    <h2> aastey backend assignment  </h2>
+    <div className="container">
+      {fake.map((values) => {
+        return (
+          <>
+          <div className="box">
+      <div className="content">
+        <h3> {values.title} </h3>
+        <p> {values.description} </p>
+      </div>
+      <img src= {values.image} alt="" />
+     </div></>
+        )
+      })}
+     
     </div>
-  );
+    
+    </>
+  )
 }
 
-export default App;
+export default App
